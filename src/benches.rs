28d525f231ec9e534ff9bff3c9c6a3f098ffde05
arturo castro <arturo@openframeworks.cc>
2017-09-25 13:41:19 +0200

    revert benches to vecstorage

--- src/benches.rs
+++ src/benches.rs
@@ -11,8 +11,8 @@ pub const N_POS_VEL: usize = 1000;
 pub const N_POS: usize = 9000;
 
 // type BenchStorage<T> = ::DenseVec<T>;
-// type BenchStorage<T> = ::VecStorage<T>;
-type BenchStorage<T> = ::AssocVec<T>;
+type BenchStorage<T> = ::VecStorage<T>;
+// type BenchStorage<T> = ::AssocVec<T>;
 // type BenchStorage<T> = ::HashMapStorage<T>;
 // type BenchStorage<T> = ::Tree<T>;
 // type BenchStorage<T> = HashMap<usize,T>;
